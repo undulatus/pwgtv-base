ccecd00e1181bccdb2845a5a65d24c171505f986
Bryan Batanes <bryan.batanes@pointwest.com.ph>
2017-04-10 12:01:36 +0800

    boot base

---
+++ src/main/java/com/pointwest/googletoken/validator/GoogleTokenValidatorApplication.java
@@ -0,0 +1,12 @@
+package com.pointwest.googletoken.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleTokenValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleTokenValidatorApplication.class, args);
+	}
+}
